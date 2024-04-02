f6ef0ec7c4f2a3ef039dd3ae925f7cc2f968c2b3
tacosz <nabilnasruddin.offc@gmail.com>
2024-04-02 18:28:42 +0700

    penambahan menampilkan data ke

--- strukturA/strukturA.cpp
+++ strukturA/strukturA.cpp
@@ -21,6 +21,7 @@ int main()
 {
     Mahasiswa mhs [3];
     for (int i = 0; i < 3; i++) {
+        cout << "Data ke-" << (i + 1) << ":" << endl;
         cout << "Nomor Mahasiswa : ";
         getline(cin, mhs[i].nim);
         cout << "Nama Mahasiswa : ";
