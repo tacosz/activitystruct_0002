46134d756ebe3d16000e7bf3bec4868616476e98
tacosz <nabilnasruddin.offc@gmail.com>
2024-04-02 17:15:38 +0700

    penambahan menampilkan nama desa dan kota

--- strukturA/strukturA.cpp
+++ strukturA/strukturA.cpp
@@ -33,6 +33,8 @@ int main()
     cout << endl;
     cout << "\n NIM : " << mhs.nim;
     cout << "\n Nama : " << mhs.nama;
-    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Alamat : ";
+    cout << "\n Desa : " << mhs.alamat.desa;
+    cout << "\n Kota : " << mhs.alamat.kota;
     cout << "\n Umur : " << mhs.umur;
 }
